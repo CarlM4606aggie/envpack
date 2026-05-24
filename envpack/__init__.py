a9d0887ca14b97d3e7037a2d2fc3e8458ca2bd2c
Carl M. <CarlM4606aggie@users.noreply.github.com>
2026-05-25 04:43:07 +0800

    feat: add get_version() helper with docstring and doctest

--- envpack/__init__.py
+++ envpack/__init__.py
@@ -2,3 +2,20 @@
 
 __version__ = "0.1.0"
 __author__ = "envpack contributors"
+
+
+def get_version() -> str:
+    """Return the current version string of envpack.
+
+    Returns
+    -------
+    str
+        The version string in PEP 440 format (e.g. ``"0.1.0"``).
+
+    Examples
+    --------
+    >>> import envpack
+    >>> envpack.get_version()
+    '0.1.0'
+    """
+    return __version__
